c7d85a24b24057c1b8c7f445237abed283858fcd
Rob Meijer <rob.meijer@immediate.co.uk>
2017-07-28 16:28:05 +0100

    Add a few more bundles

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,11 @@
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\MonologBundle\MonologBundle' => ['all' => true],
+    'Symfony\Bundle\SecurityBundle\SecurityBundle' => ['all' => true],
+    'Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle' => ['all' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
 ];
