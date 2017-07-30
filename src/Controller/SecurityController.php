efc6f3c2277213b63d09f3b2fb1702aa1c2a59e8
Rob Meijer <email@robmeijer.co.uk>
2017-07-30 15:22:41 +0100

    Enable basic login functionality

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    public function loginAction(AuthenticationUtils $authUtils)
+    {
+        return $this->render('security/login.html.twig', [
+            'last_username' => $authUtils->getLastUsername(),
+            'error' => $authUtils->getLastAuthenticationError(),
+        ]);
+    }
+
+    public function registerAction()
+    {
+        return $this->render('security/register.html.twig');
+    }
+}
