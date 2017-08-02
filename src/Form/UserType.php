c78da2deb1be045ec2284029374fe1d92fd545d7
Rob Meijer <rob.meijer@immediate.co.uk>
2017-08-02 12:48:09 +0100

    Make use of Symfony's built in form functions

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,15 +17,16 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName', TextType::class)
-            ->add('surname', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('username', TextType::class)
-            ->add('plainPassword', RepeatedType::class, array(
+            ->add('firstName', TextType::class, ['label' => false, 'attr' => ['placeholder' => 'First Name']])
+            ->add('surname', TextType::class, ['label' => false, 'attr' => ['placeholder' => 'Surname']])
+            ->add('email', EmailType::class, ['label' => false, 'attr' => ['placeholder' => 'Email']])
+            ->add('username', TextType::class, ['label' => false, 'attr' => ['placeholder' => 'Username']])
+            ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
-                'first_options'  => ['label' => 'Password'],
-                'second_options' => ['label' => 'Repeat Password'],
-            ))
+                'first_options'  => ['label' => false, 'attr' => ['placeholder' => 'Password']],
+                'second_options' => ['label' => false, 'attr' => ['placeholder' => 'Repeat Password']],
+            ])
+	        ->add('save', SubmitType::class, ['label' => 'Register', 'attr' => ['class' => 'btn-primary btn-block btn-flat']]);
         ;
     }
 
