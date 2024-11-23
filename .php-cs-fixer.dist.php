051aea86214e6bf11e8006ce3cf42cfa97c695ec
Rob Meijer <profusion@gmail.com>
2024-11-23 11:14:38 +0000

    Update php-cs-fixer config and add composer scripts

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,15 +1,17 @@
 <?php
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-    ->exclude('config')
-    ->exclude('public')
-    ->exclude('var')
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
 ;
 
 return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@PER-CS' => true,
     ])
+    ->setCacheFile(__DIR__ . '/var/cache/php-cs-fixer/.php-cs-fixer.cache')
     ->setFinder($finder)
 ;
