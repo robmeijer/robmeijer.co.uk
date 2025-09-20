5da8a465fbfbc93c618b2a5d30a794fb67b68b6a
Rob Meijer <profusion@gmail.com>
2025-09-20 10:21:42 +0100

    Update linting rules

--- app/.php-cs-fixer.dist.php
+++ app/.php-cs-fixer.dist.php
@@ -10,7 +10,13 @@ $finder = new PhpCsFixer\Finder()
 return new PhpCsFixer\Config()
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
-        '@PER-CS' => true,
+        '@Symfony' => true,
+        'class_attributes_separation' => [
+            'elements' => ['const' => 'one', 'method' => 'one', 'property' => 'one', 'trait_import' => 'one'],
+        ],
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_line_span' => ['const' => 'single', 'method' => 'single', 'property' => 'single'],
+        'single_line_empty_body' => true,
     ])
     ->setCacheFile(__DIR__ . '/var/php-cs-fixer/.php-cs-fixer.cache')
     ->setFinder($finder)
